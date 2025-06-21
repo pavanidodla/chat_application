77568649121e85790371252f45dacb81b34ac4f3
Pavani <pavani826>
2025-06-21 09:20:18 +0545

    first

--- server/server.js
+++ server/server.js
@@ -47,5 +47,11 @@ app.use("/api/messages",messageRouter);
 //connect to MongoDB
 await connectDB();
 
+if(process.env.NODE_ENV !== "production"){
+
+
+
 const PORT=process.env.PORT || 5000;
 server.listen(PORT,()=>console.log("Server is running on PORT:"+PORT));
+}
+export default server;
